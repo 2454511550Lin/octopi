998b02b9644dc0f5097ab21618be4cb2f28fb9e0
heguang <2454511550@qq.com>
2024-06-03 12:20:33 -0700

    add google cloud instructions

---
+++ google cloud script/3. start_one_gpu_vm.sh
@@ -0,0 +1,29 @@
+export INSTANCE_NAME="test" DISK_NAME="malaria-training" ZONE="us-west1-a" PROJECT="octopi-416908"
+
+gcloud compute instances create $INSTANCE_NAME \
+    --project=$PROJECT \
+    --zone=$ZONE \
+    --machine-type=n1-standard-4 \
+    --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
+    --maintenance-policy=TERMINATE \
+    --provisioning-model=STANDARD \
+    --service-account=928063271722-compute@developer.gserviceaccount.com \
+    --scopes=https://www.googleapis.com/auth/cloud-platform \
+    --accelerator=count=1,type=nvidia-tesla-t4 \
+    --tags=http-server,https-server,lb-health-check \
+    --create-disk=auto-delete=yes,boot=yes,device-name=$INSTANCE_NAME,image=projects/ml-images/global/images/c0-deeplearning-common-gpu-v20240128-debian-11-py310,mode=rw,size=100,type=projects/$PROJECT/zones/$ZONE/diskTypes/pd-balanced \
+    --disk=boot=no,device-name=train,mode=rw,name=$DISK_NAME \
+    --no-shielded-secure-boot \
+    --shielded-vtpm \
+    --shielded-integrity-monitoring \
+    --labels=goog-ec-src=vm_add-gcloud \
+    --reservation-affinity=any
+
+
+# ssh to the vm
+# set up conda environment
+
+conda create --name pt23 python=3.8
+conda activate pt23
+conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
+pip install hydra-core wandb omegaconf torch pandas tqdm scikit-learn
